dcbe1a23930911ecb352709d5659619c7335cf29
Anna Seregi <annaseregi@zoltans-mbp.home>
2021-11-26 15:48:17 +0000

    fixed navbar width

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,18 +4,20 @@ import Button from "./ToggleButton";
 const Nav = ({ toggleTheme, theme }) => {
 	return (
 		<nav id="nav" className="nav flex">
-			<ul className="nav-list flex ff-sans-cond uppercase fs-300 fw-700 letter-spacing-4">
-				<li className="nav-item">
-					<a href="#home">Home</a>
-				</li>
-				<li className="nav-item">
-					<a href="#featured">Featured Projects</a>
-				</li>
-				<li className="nav-item">
-					<a href="#projects">Other Projects</a>
-				</li>
-			</ul>
-			<Button toggleTheme={toggleTheme} theme={theme} />
+			<div className="nav-box">
+				<ul className="nav-list flex ff-sans-cond uppercase fs-300 fw-700 letter-spacing-4">
+					<li className="nav-item">
+						<a href="#home">Home</a>
+					</li>
+					<li className="nav-item">
+						<a href="#featured">Featured Projects</a>
+					</li>
+					<li className="nav-item">
+						<a href="#projects">Other Projects</a>
+					</li>
+				</ul>
+				<Button toggleTheme={toggleTheme} theme={theme} />
+			</div>
 		</nav>
 	);
 };
